bd267b5f4cf57cfa5a89368426bdd384aa82497d
n <nataliaklucnikova9@gmail.com>
2025-09-17 14:46:05 +0300

    \

--- Dashboard/React_project/React_pr/sandbox-main/src/pages/main/Main.tsx
+++ Dashboard/React_project/React_pr/sandbox-main/src/pages/main/Main.tsx
@@ -366,16 +366,31 @@ export const Main = () => {
                       <div className='card r' style={{marginTop: '24px', marginLeft: '24px', borderRadius: '18px', border:'none'}}>
                         <div className='card-body'>
                             <div className='calendar_header'>
-                              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path fill-rule="evenodd" clip-rule="evenodd" d="M11.3536 1.64645C11.5488 1.84171 11.5488 2.15829 11.3536 2.35355L5.70711 8L11.3536 13.6464C11.5488 13.8417 11.5488 14.1583 11.3536 14.3536C11.1583 14.5488 10.8417 14.5488 10.6464 14.3536L4.64645 8.35355C4.45118 8.15829 4.45118 7.84171 4.64645 7.64645L10.6464 1.64645C10.8417 1.45118 11.1583 1.45118 11.3536 1.64645Z" fill="black"/>
+                            <div className='a d-flex justify-content-between align-items-center'>
+                              <div className='circle_svg rounded-circle d-flex justify-content-center align-items-center '>
+                              <svg width="15" height="15" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" clip-rule="evenodd" d="M11.3536 1.64645C11.5488 1.84171 11.5488 2.15829 11.3536 2.35355L5.70711 8L11.3536 13.6464C11.5488 13.8417 11.5488 14.1583 11.3536 14.3536C11.1583 14.5488 10.8417 14.5488 10.6464 14.3536L4.64645 8.35355C4.45118 8.15829 4.45118 7.84171 4.64645 7.64645L10.6464 1.64645C10.8417 1.45118 11.1583 1.45118 11.3536 1.64645Z" />
                               </svg>
+                              </div>
                               <p>September 2025</p>
-                              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path fill-rule="evenodd" clip-rule="evenodd" d="M4.64645 1.64645C4.84171 1.45118 5.15829 1.45118 5.35355 1.64645L11.3536 7.64645C11.5488 7.84171 11.5488 8.15829 11.3536 8.35355L5.35355 14.3536C5.15829 14.5488 4.84171 14.5488 4.64645 14.3536C4.45118 14.1583 4.45118 13.8417 4.64645 13.6464L10.2929 8L4.64645 2.35355C4.45118 2.15829 4.45118 1.84171 4.64645 1.64645Z" fill="black"/>
+                              <div className='circle_svg rounded-circle d-flex justify-content-center align-items-center '>
+                              <svg width="15" height="15" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" clip-rule="evenodd" d="M4.64645 1.64645C4.84171 1.45118 5.15829 1.45118 5.35355 1.64645L11.3536 7.64645C11.5488 7.84171 11.5488 8.15829 11.3536 8.35355L5.35355 14.3536C5.15829 14.5488 4.84171 14.5488 4.64645 14.3536C4.45118 14.1583 4.45118 13.8417 4.64645 13.6464L10.2929 8L4.64645 2.35355C4.45118 2.15829 4.45118 1.84171 4.64645 1.64645Z" />
                               </svg>
-
+                              </div>
                             </div>
-                            <div className='calendar_week'></div>
+                            </div>
+                            <div className='calendar_week'>
+                              <div className='week'>
+                                <p>Su</p>
+                                <p>Mo</p>
+                                <p>Tu</p>
+                                <p>We</p>
+                                <p>Th</p>
+                                <p>Fr</p>
+                                <p>Sa</p>
+                              </div>
+                            </div>
                             <div className='days'></div>
                         </div>
                       </div>
